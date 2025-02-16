9e5f907dbdbcffd52509b149f758678ac203e113
refthatsafoul <joshuargonzaga07@gmail.com>
2025-02-16 14:40:14 -0800

    player movement

--- Scenes/Entities/Player/player.gd
+++ Scenes/Entities/Player/player.gd
@@ -1,39 +1,49 @@
 extends CharacterBody2D
-
+#reeferences to the animated sprite
 @onready var anim =$Sprite
+#brings the animation of the sprite into this script
+@export var speed : float = 20.0 # sets how fast our character moves
 
+var is_moving : bool = false #tells if the character is moving or not
+var dir : String = "none" #string that will help us with animation
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-var last_direction: Vector2
-
-func _physics_process(delta: float) -> void:
-	# A
-
-	# Ha
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
+func _process(delta):
 	
+	if Input.is_action_just_pressed("up"): #all of these control the movement 
+		velocity = Vector2.UP * speed
+		is_moving = true
+		dir = "up"
+	elif Input.is_action_pressed("down"):
+		velocity = Vector2.DOWN * speed
+		is_moving = true
+		dir = "down"
+	elif Input.is_action_pressed("left"):
+		velocity = Vector2.LEFT * speed
+		is_moving = true
+		dir = "left"
+	elif Input.is_action_pressed("right"):
+		velocity = Vector2.RIGHT * speed
+		is_moving = true
+		dir = "right"
+		
+	move_and_slide() # ensure it moves actually 
 	
-	if direction:
-		velocity.x = direction.x * SPEED 
-		last_direction = direction
-		velocity.y = direction.y *SPEED 
-		last_direction = direction
-		
-	else:
-		velocity = Vector2.ZERO
-	if velocity == Vector2.ZERO and last_direction:
-		if last_direction== Vector2.UP: #which is down
-			anim.play("idle forward")
-		elif last_direction==Vector2.DOWN:
-			anim.play("idle back")
-		elif last_direction==Vector2.RIGHT:
-			anim.play("idle right")
-		elif last_direction == Vector2.LEFT:
-			anim.play("idle left")
-		
-
-	move_and_slide()
+	if is_moving == true: #does animation based on which direction the character is facing
+		if dir == "left":
+			anim.play("walking_left")
+		elif dir == "right":
+			anim.play("walking_right")
+		elif dir == "up":
+			anim.play("walking_up")
+		elif dir == "down":
+			anim.play("walking_down")
+	
+	elif is_moving == false: #does idle animation for when the player is not moving
+		if dir == "left":
+			anim.play("idle_left")
+		elif dir == "right":
+			anim.play("idle_right")
+		elif dir == "up":
+			anim.play("idle_back")
+		elif dir == "down":
+			anim.play("idle_forward")

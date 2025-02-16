86f4495bc2da8be8076fd739b9328f603600d297
Oktim89 <111383569+Oktim89@users.noreply.github.com>
2025-02-16 22:52:59 +0000

    Player Character Movement

--- Scenes/Entities/Player/player.gd
+++ Scenes/Entities/Player/player.gd
@@ -6,25 +6,32 @@ extends CharacterBody2D
 
 var is_moving : bool = false #tells if the character is moving or not
 var dir : String = "none" #string that will help us with animation
+var last_direction:Vector2
 
 func _process(delta):
 	
-	if Input.is_action_just_pressed("up"): #all of these control the movement 
+	if Input.is_action_pressed("up"): #all of these control the movement 
 		velocity = Vector2.UP * speed
 		is_moving = true
 		dir = "up"
+		last_direction= Vector2.UP
 	elif Input.is_action_pressed("down"):
 		velocity = Vector2.DOWN * speed
 		is_moving = true
 		dir = "down"
+		last_direction= Vector2.DOWN
 	elif Input.is_action_pressed("left"):
 		velocity = Vector2.LEFT * speed
 		is_moving = true
 		dir = "left"
+		last_direction= Vector2.LEFT
 	elif Input.is_action_pressed("right"):
 		velocity = Vector2.RIGHT * speed
 		is_moving = true
 		dir = "right"
+		last_direction= Vector2.RIGHT
+	else:
+		velocity= Vector2.ZERO
 		
 	move_and_slide() # ensure it moves actually 
 	
@@ -38,12 +45,13 @@ func _process(delta):
 		elif dir == "down":
 			anim.play("walking_down")
 	
-	elif is_moving == false: #does idle animation for when the player is not moving
-		if dir == "left":
-			anim.play("idle_left")
-		elif dir == "right":
-			anim.play("idle_right")
-		elif dir == "up":
+	
+	if velocity == Vector2.ZERO and last_direction:
+		if last_direction== Vector2.UP: #which is down
 			anim.play("idle_back")
-		elif dir == "down":
+		elif last_direction==Vector2.DOWN:
 			anim.play("idle_forward")
+		elif last_direction==Vector2.RIGHT:
+			anim.play("idle_right")
+		elif last_direction == Vector2.LEFT:
+			anim.play("idle_left")

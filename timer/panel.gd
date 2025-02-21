945b99f24d60db3434b514ad8062bcafe975f977
Oktim89 <111383569+Oktim89@users.noreply.github.com>
2025-02-21 01:01:30 +0000

    updated title screen with music

--- timer/panel.gd
+++ timer/panel.gd
@@ -1,9 +1,16 @@
 extends Panel
-
+@onready var hand =$Sprite2D/TextureRect
 var time: float = 0.0
 var minutes : int = 0
 var seconds : int = 0 
+var spring = 200.0
+var damp = 10.0
+var velocity = 15.0
 
+
+func _ready() -> void:
+	pass
+	
 func _process(delta) -> void:
 	time+= delta
 	seconds = fmod(time, 60)
@@ -17,3 +24,13 @@ func stop() -> void:
 	
 func get_time_formatted() -> String:
 	return "%02d:%02d.%03d" % [minutes, seconds]
+
+
+func _on_timer_timeout() -> void:
+	#DampedOscillator.animate(hand,"rotation",spring,damp*2,velocity,2)
+	on_press_me_pressed()
+
+func on_press_me_pressed():
+	hand.rotation_degrees+=90
+	DampedOscillator.animate(hand,"rotation",spring,damp*2,velocity*1.2,2)
+	

8755bd7785ad5b7571b84944c6f3dd738bba6e9e
Oktim89 <111383569+Oktim89@users.noreply.github.com>
2025-02-22 10:59:19 +0000

    almost finished

--- Scenes/UI/Shop/pizza_tab.gd
+++ Scenes/UI/Shop/pizza_tab.gd
@@ -1,15 +1,12 @@
-extends Control
+extends TabBar
 
-@onready var text=$HBoxContainer/TextureRect/RichTextLabel
+@onready var slide_controller =$RichTextLabel/Control
+@onready var slider = $Scroller
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	update_money_text()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	update_money_text()
-	pass
-func update_money_text()->void:
-	text.text=str(Globals.money)
+	slide_controller.global_position.x = -slider.value
